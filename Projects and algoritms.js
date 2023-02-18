9fc262730f558776b6d9c0261aeb019d1816df4d
Марк22 <alexbrega55805@gmail.com>
2023-02-18 23:23:34 +0200

    Update Projects and algoritms.js

--- Projects and algoritms.js
+++ Projects and algoritms.js
@@ -212,3 +212,21 @@ function validBraces(braces){
   }
   return braces == ''
 }
+
+
+//ограничивает количество повторений в массиве
+function deleteNth(arr,n){
+  const a = {}
+  const result = [];
+  for(let i = 0; i < arr.length; i++){
+    if(!a[arr[i]]){
+      a[arr[i]] = 1
+    }else{
+      a[arr[i]] += 1
+    }
+    if(a[arr[i]]<=n){
+      result.push(arr[i])
+    }
+  }
+  return result
+}
